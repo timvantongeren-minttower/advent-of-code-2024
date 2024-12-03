f438a3c5bbeadd3029efdc35b2dc353bb4b07649
timvantongeren <timvantongeren@minttower.com>
2024-12-03 17:26:38 +0100

    part 2

--- day3/solution.py
+++ day3/solution.py
@@ -28,5 +28,27 @@ def get_answer_to_part_1(input_stream: io.StringIO) -> int:
     return sum(a * b for a, b in operations)
 
 
+def get_all_instructions(line: str) -> list[str]:
+    regex_pattern = r"(mul\([0-9]*,[0-9]*\)|do\(\)|don't\(\))"
+    return re.findall(regex_pattern, line)
+
+
 def get_answer_to_part_2(input_stream: io.StringIO) -> int:
-    pass
+    lines = input_stream.readlines()
+    instructions = []
+    for line in lines:
+        instructions.extend(get_all_instructions(line))
+
+    enabled = True
+    valid_operations = []
+    for instruction in instructions:
+        if "don't" in instruction:
+            enabled = False
+        elif "do" in instruction:
+            enabled = True
+        else:
+            # is mul
+            if not enabled:
+                continue
+            valid_operations.append(parse_tuple_from_operation(instruction))
+    return sum(a * b for a, b in valid_operations)
