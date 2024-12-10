fb2ab9d02b2028737235815f8d7f7805d8ee00bd
timvantongeren <tvtongeren12@gmail.com>
2024-12-10 20:10:47 +0100

    bug == feature lol

--- day10/solution.py
+++ day10/solution.py
@@ -15,8 +15,21 @@ class PeakCounter:
         return len(self.peaks)
 
 
+@dataclass
+class PeakCounter2:
+    peak_count: int = 0
+
+    def add(self, peak: tuple[int, int]):
+        self.peak_count += 1
+
+    def number_of_peaks(self) -> int:
+        return self.peak_count
+
+
 def explore(
-    starting_location: tuple[int, int], the_map: list[list[int]], peak_counter: PeakCounter
+    starting_location: tuple[int, int],
+    the_map: list[list[int]],
+    peak_counter: PeakCounter | PeakCounter2,
 ):
     n, m = len(the_map), len(the_map[0])
     current_height = the_map[starting_location[0]][starting_location[1]]
@@ -48,4 +61,13 @@ def get_answer_to_part_1(input_stream: io.StringIO) -> int:
 
 
 def get_answer_to_part_2(input_stream: io.StringIO) -> int:
-    pass
+    lines = input_stream.readlines()
+    the_map = [[int(i) for i in line.replace("\n", "")] for line in lines]
+    n, m = len(the_map), len(the_map[0])
+    trailheads = [(i, j) for i in range(n) for j in range(m) if the_map[i][j] == 0]
+    total = 0
+    for trailhead in trailheads:
+        peak_counter = PeakCounter2()
+        explore(trailhead, the_map, peak_counter)
+        total += peak_counter.number_of_peaks()
+    return total
